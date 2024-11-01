c1ac52e365b59d007c290fb1cd885c17867fae3e
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-11-01 18:05:18 +0100

    Completed header & some animations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./Styles/styles.scss";
 import * as nodes from "./Modules/selectors";
 import { createElement } from "./Modules/functions";
+
+const avatar = document.querySelector('div.avatar');
+avatar.addEventListener
+avatar.onmouseover = flowanimation;
+
+function flowanimation() {
+  const compStyle = window.getComputedStyle(avatar);
+  const initialAngle = compStyle.getPropertyValue('--angle');
+  const motionInt = setInterval(rotate, 10);
+  function rotate() {
+    const angle = +avatar.style.getPropertyValue('--angle') || +initialAngle;
+    avatar.style.setProperty('--angle', `${angle + 1}`);
+  }
+  avatar.onmouseout = function() {
+    clearInterval(motionInt);
+  }
+}
